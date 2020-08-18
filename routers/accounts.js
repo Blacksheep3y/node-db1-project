d65f13f00e26285d123d28da17bec34050824916
Matt C <Blacksheep3y@gmail.com>
2020-08-18 18:42:23 -0400

    started working on server file and building out account

---
+++ routers/accounts.js
@@ -0,0 +1,7 @@
+const express = require("express") // import express
+const router = express.Router() //import the router from express method
+const db = require('../data/dbConfig'); //import database
+
+
+
+module.exports = router;
