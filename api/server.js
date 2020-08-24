e3d6d2cf86f197593e6b810c74922572ccc2f493
Matt C <Blacksheep3y@gmail.com>
2020-08-23 20:57:21 -0400

    Finished MVP

--- api/server.js
+++ api/server.js
@@ -1,5 +1,5 @@
 const express = require("express"); // import express
-const accountsRouter = require('../router/accounts') // import router
+const accountsRouter = require('../routers/accounts') // import router
 
 const db = require("../data/dbConfig.js"); // require database
 
@@ -7,7 +7,7 @@ const server = express(); // allow server to use express
 
 server.use(express.json()); // convert express to json
 
-// server.use('/api/accounts', accountsRouter) // set accounts router api to this endpoint to start
+server.use('/api/accounts', accountsRouter) // set accounts router api to this endpoint to start
 
 server.get('/', (req, res) => {
     res.status(200).json({ API: 'The API is Running!' })
